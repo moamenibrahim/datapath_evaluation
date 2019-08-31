1a02be0b3da84df911379eca9a5b7e15fa511952
Moamen Ibrahim <mmibrahem76@gmail.com>
2019-08-31 09:57:16 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import re
+from os import listdir
+import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
+from scipy.stats import circstd
+
+# declare main data directories
+measured_directory = 'data/measured/'
+truth_directory = 'data/truth/'
+
+# load all files in data directory (measured is enough for our algorithm)
+files = listdir(measured_directory)
+
+for file in files:
+
+    # file number to fetch the correct corresponding file
+    file_number = (re.findall('\d+', file))
+
+    # Load measure components
+    I_stream, Q_stream, sensor_phase_bias = np.load(
+        measured_directory + file, allow_pickle=True, fix_imports=True, encoding='bytes')
+
+    # load ground truth phase
+    phase_groundtruth = np.load(
+        truth_directory + 'data_phase_ground_truth_' + file_number[0] + '.npy')
+
+    # The original basic datapath applied to data
+    measured_phase = -np.arctan2(Q_stream, I_stream)  # compute phase
+    filtered_phase = []
+
+    # Filter noise in phase images
+    for image in measured_phase:
+        filtered_phase.append(ndimage.gaussian_filter(image, 4))
+    filtered_phase = np.asarray(filtered_phase)
+
+    # Compute noise and error
+    phase_error = np.mean(filtered_phase - phase_groundtruth, axis=0)
+    phase_noise = circstd(filtered_phase, axis=0)
+
+    # Report phase error and noise
+    fig = plt.figure()
+    plt.imshow(phase_error)
+    plt.title("phase error")
+    plt.colorbar()
+    fig.savefig("results/error/phase_error_report_" + file_number[0])
+
+    fig = plt.figure()
+    plt.imshow(phase_noise)
+    plt.colorbar()
+    plt.title("phase noise")
+    fig.savefig("results/noise/phase_noise_report_" + file_number[0])
