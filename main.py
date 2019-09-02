7148ef6f3efbe683364966c9403a998520b8b373
Moamen Ibrahim <mmibrahem76@gmail.com>
2019-09-02 09:46:11 +0300

    Add support for argument parsing
    Support running python script with extra arguments
    for modular directory injection for both measured
    and truth findings.
    
    This approach can be expanded furthermore to support
    more modular implementations as extra features.

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import re
+import argparse
 from os import listdir
 from src.datapath_evaluate import DataPathEvaluate
 
-# declare main data directories
-measured_directory = 'data/measured/'
-truth_directory = 'data/truth/'
 
-# declare a child class
-dataPathEvaluate = DataPathEvaluate(measured_directory, truth_directory)
+def main(measured_directory, truth_directory):
+    # declare a child class with updated directories
+    dataPathEvaluate = DataPathEvaluate(measured_directory, truth_directory)
 
-# load all files in data directory (measured is enough for our algorithm)
-files = listdir(measured_directory)
+    # load all files in data directory (measured is enough for our algorithm)
+    files = listdir(measured_directory)
 
-for file in files:
+    for file in files:
+        if file.endswith('npy'):
+            # file number to fetch the correct corresponding file
+            file_number = (re.findall('\d+', file))
+            # Load measure components
+            I_stream, Q_stream, sensor_phase_bias, phase_groundtruth = dataPathEvaluate.load_components(
+                file, file_number)
 
-    if file.endswith('npy'):
+            # Calculate phase error and noise
+            phase_error, phase_noise = dataPathEvaluate.compute_values(
+                I_stream, Q_stream, phase_groundtruth)
 
-        # file number to fetch the correct corresponding file
-        file_number = (re.findall('\d+', file))
-        # Load measure components
-        I_stream, Q_stream, sensor_phase_bias, phase_groundtruth = dataPathEvaluate.load_components(
-            file, file_number)
+            # Report phase error and noise
+            dataPathEvaluate.report_results(
+                phase_error, phase_noise, file_number)
 
-        # Calculate phase error and noise
-        phase_error, phase_noise = dataPathEvaluate.compute_values(
-            I_stream, Q_stream, phase_groundtruth)
 
-        # Report phase error and noise
-        dataPathEvaluate.report_results(phase_error, phase_noise, file_number)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--measured", default='data/measured/',
+                        help="directory of the measured values")
+    parser.add_argument("--truth", default='data/truth/',
+                        help="directory of truth values")
+    args = parser.parse_args()
+
+    measured_directory = args.measured
+    truth_directory = args.truth
+    print("Processing...")
+    main(measured_directory, truth_directory)
